7e3f82964195d9025b22f78419406f65f845b1d3
eclipseCJP <cris.tecwordd@gmail.com>
2023-12-18 20:20:35 -0400

    implementando jogo forca

--- jogoForca.py
+++ jogoForca.py
@@ -3,7 +3,26 @@ def jogar():
     print("Bem vindo ao jogo da Forca!")
     print("*********************************")
 
+    palavra_secreta = "banana"
 
+    enforcou = False
+    acertou = False
+
+#enquanto true 
+    while(not enforcou and not acertou):
+
+        chute = input("Qual letra??")
+        #devolvendo a minha string sem espaços
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            #upper()deixando a minha letra em maiuscula
+            if(chute.upper() == letra.upper()):
+                print(f"encontrei a letra {letra} na posicao {index}")
+            index+=1
+
+        print("jogando...")
 
     print("Fim do jogo")   
      
